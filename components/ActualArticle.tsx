4e93c7eb257a77f6593deeea512692f04494c923
Sadullo <sadullo@gmail.com>
2023-06-08 10:23:46 +0500

    feat: add home layout

---
+++ components/ActualArticle.tsx
@@ -0,0 +1,9 @@
+export default function ActualArticle() {
+  return (
+    <article className="actual-article">
+      <h3 className="actual-article__title">
+        O‘zbekiston va Singapur o‘rtasida 7 ta hamkorlik hujjati imzolandi
+      </h3>
+    </article>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import banner from '@/assets/images/banner.png';
 import Article from '@/components/Article';
+import ActualArticle from '@/components/ActualArticle';
 
 export default function Home() {
   return (
@@ -11,13 +12,41 @@ export default function Home() {
       </Head>
       <section className="home">
         <div className="container">
-          <div className="intro">
-            <div className="intro__slider">
+          <div className="intro main-grid">
+            <div className="intro__slider main-grid__item1">
               <Image src={banner} alt="slider image" />
             </div>
-            <div className="intro__articles">
+            <div className="intro__articles main-grid__item2">
               <Article />
               <Article />
+            </div>
+          </div>
+          <div className="main-grid">
+            <div className="latest-news main-grid__item1">
+              <h2 className="latest-news__title">Eng so&#39;ngi yangiliklar</h2>
+              <div className="latest-news__grid">
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+              </div>
+            </div>
+            <div className="actual-news main-grid__item2">
+              <h3 className="actual-news__title">Dolzarb</h3>
+              <div className="actual-news__grid">
+                <ActualArticle />
+                <ActualArticle />
+                <ActualArticle />
+                <ActualArticle />
+                <ActualArticle />
+                <ActualArticle />
+                <ActualArticle />
+              </div>
             </div>
           </div>
         </div>
