a383c5afc60cdc62bfd4a083357e18cc2ad240d5
Sadullo <sadullo@gmail.com>
2023-06-08 12:21:55 +0500

    feat: add reponsive home ui

--- components/Article.tsx
+++ components/Article.tsx
@@ -5,13 +5,13 @@ export default function Article() {
   return (
     <article className="article">
       <Image src={articleImg} alt="article photo" className="article__photo" />
-      <h3 className="article__title">
-        O‘zbekiston va Singapur o‘rtasida 7 ta hamkorlik hujjati imzolandi
-      </h3>
-      <div className="article__caption">
-        <span>571 kishi o‘qidi</span>
-        <span>/</span>
-        <span>22.05.2023</span>
+      <div className="article__body">
+        <h3 className="article__title">Aliment bu, farzand oldidagi majburiyat</h3>
+        <div className="article__caption">
+          <span>571 kishi o‘qidi</span>
+          <span>/</span>
+          <span>22.05.2023</span>
+        </div>
       </div>
     </article>
   );
