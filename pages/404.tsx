af77e74aa8641a81dee0dce401a5a3e23fc9e164
Sadullo <sadullo@gmail.com>
2023-06-11 00:55:38 +0500

    fix: fix responsive 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,12 +3,14 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <section className="not-found">
-      <div className="not-found__inner">
-        <h2>404</h2>
-        <p>This page could not be found.</p>
-        <Link href="/" className="not-found__link">
-          Asosiy sahifaga o`tish
-        </Link>
+      <div className="container">
+        <div className="not-found__inner">
+          <h2>404</h2>
+          <p>This page could not be found.</p>
+          <Link href="/" className="not-found__link">
+            Asosiy sahifaga o`tish
+          </Link>
+        </div>
       </div>
     </section>
   );
