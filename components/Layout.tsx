24975fd844f26621e6e2eff5ac1346b2eaeea49a
Sadullo <sadullo@gmail.com>
2023-07-12 12:20:55 +0500

    fix: remove test mode text

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,9 +7,6 @@ export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
       <Header />
-      <div className="test__wrapper">
-        <p className="test">«Bobotogʻ tongi» gazetasi veb sayti test rejimida ishlayapti!</p>
-      </div>
       <main>{children}</main>
       <Footer />
     </>
